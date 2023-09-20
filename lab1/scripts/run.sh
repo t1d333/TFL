8574c8179d852f82b6ddb3d5c7bead5a081b4e56
Kirill Kiselev <lirikk285@mail.ru>
2023-09-20 22:57:09 +0300

    fix run script

--- lab1/scripts/run.sh
+++ lab1/scripts/run.sh
@@ -5,7 +5,6 @@ OUTPUT_FILE="./out.smt2"
 PREAMBLE_FILE="./templates/preamble.smt2"
 END_FILE="./templates/end.smt2"
 
-echo $SMT_CODE
 cat $PREAMBLE_FILE > $OUTPUT_FILE 
 cargo run -q < "$1" >> $OUTPUT_FILE
 cat $END_FILE >> $OUTPUT_FILE
